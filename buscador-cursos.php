6cfc2e21b505e6ad44736a29594019bfd732386b
Matheus <mat.hen.gra@gmail.com>
2023-10-12 17:05:11 -0300

    install css-selector

--- buscador-cursos.php
+++ buscador-cursos.php
@@ -1,5 +1,25 @@
 <?php
 
+/* 
+    COMPOSER
+    Gerenciador de pacotes
+
+    PACKAGIST
+    Respositório de pacotes públicos do Composer
+
+    >composer init
+    comando para criar o arquivo composer.json
+
+    >composer require "vendor/package"
+    comando para instalar o pacote x
+
+    >composer install
+    comando para instalar os pacotes listados no composer.lock
+
+    >composer update
+    comando para instalar e atualizar os pacotes listados no composer.json
+*/
+
 use GuzzleHttp\Client;
 
 $client = new Client();
