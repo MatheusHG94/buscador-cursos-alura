4025cae21573bfdf7cc4d2d34dab509fe2dac47d
Matheus <mat.hen.gra@gmail.com>
2023-10-18 15:37:42 -0300

    update comment

--- buscador-cursos.php
+++ buscador-cursos.php
@@ -7,20 +7,43 @@
     PACKAGIST
     Respositório de pacotes públicos do Composer
 
+    AUTOLOAD
+    O autoload no composer.json pode receber entradas psr-4, classmap e files
+
+    COMANDOS
     >composer init
     comando para criar o arquivo composer.json
 
     >composer require "vendor/package"
     comando para instalar o pacote x
 
+    >composer require --dev "vendor/package"
+    comando para instalar o pacote x para ambiente de desenvolvimento
+
     >composer install
     comando para instalar os pacotes listados no composer.lock, nas exatas versões definidas
+
+    >composer install --no-dev
+    comando para instalar as dependências de produção, não as de desenvolvimento
 
     >composer update
     comando para instalar e atualizar os pacotes listados no composer.json, nas versões declaradas ou mais atuais
 
     >composer dumpautoload
     comando para atualizar o autoload
+
+    >composer list
+    lista de comandos do composer
+
+    PACOTES DE FERRAMENTAS
+    PHPUnit
+    pacote para testes
+
+    PHPCS - PHP Code Sniffer
+    pacote para verificar se o código está de acordo com padrões, e.g. PSR12
+
+    Phan
+    pacote para verificar erros nos código
 */
 
 require_once 'vendor/autoload.php';
